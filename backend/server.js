038ffbc1ade7f6df52bfd38bb33d3175f6077349
Ron8075Z <sigma4726spring@gmail.com>
2021-06-17 21:31:59 -0500

    Using Redux state into HomeScreen(useDispatch & useSelector)

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import colors from 'colors'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
-import producRoutes from './routes/productRoutes.js'
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
   res.send('API is running...')
 })
 
-app.use('/api/products', producRoutes)
+app.use('/api/products', productRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
